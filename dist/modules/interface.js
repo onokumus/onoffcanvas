1f4e4453ef016e4c293e99b1e6aed91f862ac92c
onokumus <onokumus@gmail.com>
2020-04-13 00:07:27 +0300

    no need interface js

--- dist/modules/interface.js
+++
@@ -1,9 +0,0 @@
-/*!
-* onoffcanvas - v2.2.3
-* An offcanvas plugin
-* https://github.com/onokumus/onoffcanvas
-*
-* Made by onokumus <onokumus@gmail.com> (https://github.com/onokumus)
-* Under MIT License
-*/
-import { a as __assign } from './chunk-6fed8d7d.js';
